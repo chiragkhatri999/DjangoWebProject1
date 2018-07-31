d80f562c65adecbb48fb55292196fdd75b04e8d6
chiragkhatri999 <35372905+chiragkhatri999@users.noreply.github.com>
2018-07-31 20:09:08 +0530

    Create initial Django App

--- DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoWebProject1/DjangoWebProject1/urls.py
@@ -3,19 +3,21 @@ Definition of urls for DjangoWebProject1.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', DjangoWebProject1.views.home, name='home'),
-    # url(r'^DjangoWebProject1/', include('DjangoWebProject1.DjangoWebProject1.urls')),
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    #url(r'^$', DjangoWebProject1.views.home, name='home'),
+    #url(r'^DjangoWebProject1/', include('DjangoWebProject1.DjangoWebProject1.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    #Uncomment the next line to enable the admin:
+    #url(r'^admin/', include(admin.site.urls)),
 ]
