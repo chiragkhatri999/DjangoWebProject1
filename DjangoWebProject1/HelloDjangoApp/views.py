d480f88868e74610066d0f024149438da28ce67b
chiragkhatri999 <35372905+chiragkhatri999@users.noreply.github.com>
2018-07-31 20:24:08 +0530

    used render function to get http response

--- DjangoWebProject1/HelloDjangoApp/views.py
+++ DjangoWebProject1/HelloDjangoApp/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp\index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
